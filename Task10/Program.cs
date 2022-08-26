e9905bded0cf2ac98bbbe95ab0d5251ea9f299a1
Yuliya Borovaya <Liana-rus81@mail.ru>
2022-08-26 22:04:03 +0300

    Вывод второй цифры в трехзначном числе

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(a);
+Console.WriteLine("вторая цифра числа "+stringNumber[1]);
