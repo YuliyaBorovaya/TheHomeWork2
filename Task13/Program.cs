0d6d10f7e49198025b4de3627ad2d4313a91c999
Yuliya Borovaya <Liana-rus81@mail.ru>
2022-08-26 22:19:57 +0300

    Решение задачи по выводу третьей цифры числа

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+string aText = Convert.ToString(a);
+if (aText.Length > 2)
+{
+    Console.WriteLine("Третья цифра " + aText[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
