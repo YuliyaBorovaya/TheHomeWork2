b4f21b3740842533e92e4715152f5a86ef90400d
Yuliya Borovaya <Liana-rus81@mail.ru>
2022-08-26 22:40:06 +0300

    Проверка дня недели

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Поздравляем, можно отдыхать, этот день - выходной!");
+}
+else if (a < 1 || a > 7)
+{
+    Console.WriteLine("такого дня в неделе нет");
+}
+else 
+{
+    Console.WriteLine("Еще не выходной, работаем");
+}
